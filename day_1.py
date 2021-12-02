5bd3791e5f274b3393384cae0589449aec333fc3
Nicolas Rothenhäusler <nicolas.rothenhaeusler@statnett.no>
2021-12-02 10:35:58 +0100

    Day 1 part 2

--- day_1.py
+++ day_1.py
@@ -1,13 +1,24 @@
+import collections
 
 increased = 0
 
 with open('day_1.in') as f:
+    # double-ended queues can be very efficient for moving windows
+    # because of fast append/pop
+    depths_in_window = collections.deque()
     depths = map(int, f)
-    prev_depth = next(depths)
+    depths_in_window.append(next(depths))
+    depths_in_window.append(next(depths))
+
+    prev_avg_depth = 0
+    print(depths_in_window)
     for depth in depths:
-        if depth > prev_depth:
+        depths_in_window.append(depth)
+        avg_depth = sum(depths_in_window) / 3
+        if avg_depth > prev_avg_depth:
             increased += 1
-        prev_depth = depth
+        prev_avg_depth = avg_depth
+        print(depths_in_window)
+        depths_in_window.popleft()
 
-
-print(increased)
+print(increased - 1)  # start is edge case
